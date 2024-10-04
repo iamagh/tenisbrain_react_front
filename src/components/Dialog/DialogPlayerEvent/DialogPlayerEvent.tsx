3e77a65a52bfe5525d5f2989c9f290c1c984af33
iamagh <codesmartass.dev@gmail.com>
2024-10-04 15:34:22 -0500

    fixed

--- src/components/Dialog/DialogPlayerEvent/DialogPlayerEvent.tsx
+++ src/components/Dialog/DialogPlayerEvent/DialogPlayerEvent.tsx
@@ -7,7 +7,7 @@ import NavItem2 from "components/NavItem2";
 import { loadingMessage, convertDateStringToYYMMDD } from "utils/others";
 import { toast } from "react-toastify";
 import { getAllAvailabilitys } from "services/player/availabilitys";
-import { deleteEvent, updateEvent,createEvent, getAllEventSeries } from "services/player/events";
+import { deleteEvent, updateEvent, createEvent, getAllEventSeries } from "services/player/events";
 import { getAllMembers } from "services/player/members";
 import { getAllCoaches } from "services/player/coches";
 import ChatButton from "components/Chatting/ChatButton";
@@ -27,6 +27,8 @@ import { _Player } from "dataTypes/Player";
 
 const stripePromise: Promise<Stripe | null> = loadStripe(`${process.env.REACT_APP_STRIPE_KEY}`);
 
+
+// const paidProductsInfo = await getProductStatusForPlayer(player_id).then((res) => setPaidProducts(res?.products || []));
 export interface DialogCoachEventProps {
   isOpen: boolean;
   data?: any;
@@ -42,7 +44,6 @@ export interface DialogCoachEventProps {
 const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onClose, setIsOpenChatRooms, rooms, setRoomInfo, products, stripePromise }) => {
 
 
-  console.log("*&&^%", data)
   const player_id = localStorage.getItem("user") || "0";
   const [loading, setLoading] = useState<boolean>(false);
   const [tabActive, setTabActive] = useState('detail');
@@ -58,8 +59,9 @@ const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onCl
   const [availabilitys, setAvailabilitys]: any = useState([]);
   const [paidProducts, setPaidProducts] = useState<any>([]);
   const [selectedPaidProduct, setSelectedPaidProduct] = useState<any>()
-  const paidProductsInfo = getProductStatusForPlayer(player_id).then((res) => setPaidProducts(res?.products || []));
+  
 
+  const [pageState, setPageState] = useState<'loading' | 'submmiting' | 'ready' | 'error'>('ready');
   const coach = useSelector((state: RootState) => state.player.playerCoach);
 
   const tabs: { [key: string]: JSX.Element | null } = {
@@ -73,6 +75,61 @@ const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onCl
     />,
     "close": null
   }
+
+  const ReadyButtons = () => {
+
+    return (
+      <>
+        <ButtonPrimary
+          onClick={handleSubmit}
+          sizeClass="px-4 py-2 sm:px-5"
+        >
+          Apply
+        </ButtonPrimary>
+        <ButtonSecondary sizeClass="px-4 py-2 sm:px-5" onClick={handleDelete}>Delete</ButtonSecondary>
+        <ButtonThird
+          onClick={() => {
+            onClose();
+          }}
+          sizeClass="px-4 py-2 sm:px-5"
+        >
+          Cancel
+        </ButtonThird>
+      </>
+    )
+  }
+
+  const SubmmitingButtons = () => {
+
+    return (
+      null
+    )
+  }
+
+  const LoadingButtons = () => {
+
+    return (
+      <div className="flex items-center justify-center w-full ">
+        <ButtonPrimary>
+          <svg aria-hidden="true" role="status" className="inline w-8 h-8 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2" />
+          </svg>
+        </ButtonPrimary>
+      </div>
+    )
+  }
+
+
+  const buttons: { [key: string]: JSX.Element | null } = {
+    "loading": <LoadingButtons />,
+    "submmiting": <SubmmitingButtons />,
+    "ready": <ReadyButtons />,
+  }
+
+
+
+
 
   const closeStripeModal = () => {
     setOpenStripeModal(false)
@@ -110,7 +167,7 @@ const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onCl
       console.log("DialogPlayerEvent availabilitys filteredAvails , formatedAvails", filteredAvails, formatedAvails);
         formatedAvails.sort((a: any, b: any) => a.time?.localeCompare(b.time));
         setAvailabilitys(formatedAvails);
-        
+
         setSelectedProduct(products.find((product: any) => product.id == data.product_id) || products[0]);
         setPaidTxId(data.id ? data.players[0]?.transaction_id : null)
 
@@ -127,11 +184,10 @@ const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onCl
       }
 
       fetchData();
-
       return () => {
         setTabActive('detail');
+        setPageState("ready");
       }
-
     }, [isOpen])
 
 
@@ -291,22 +347,22 @@ const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onCl
   }
 
   const handleDelete = async () => {
-    // if (!data.id) {
-    //   toast.error('Can not Delete');
-    //   return;
-    // }
+    if (!data.id) {
+      toast.error('Can not Delete');
+      return;
+    }
     try {
       setLoading(false);
-      // const coach: any = coaches.find((item: any) => item.id == data.coach_id);
-      // const startTime = new Date(data.start);
-      // const currentTime = new Date();
-      // const hoursDiff = (startTime.getTime() - currentTime.getTime()) / (1000 * 60 * 60);
+      
+      const startTime = new Date(data.start);
+      const currentTime = new Date();
+      const hoursDiff = (startTime.getTime() - currentTime.getTime()) / (1000 * 60 * 60);
       // if (hoursDiff < coach.notice_period_for_cancellation) {
       //   toast.error(`You need ${coach.notice_period_for_cancellation} hours to cancel.`);
       //   return;
       // }
-      // const res = await deleteEvent(data.id);
-      // if (res) toast.success('The event has been removed');
+      const res = await deleteEvent(data.id);
+      if (res) toast.success('The event has been removed');
     } catch (err: any) {
       toast.error(err.message || 'The error is occur while hanlding')
     }
@@ -377,34 +433,11 @@ const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onCl
                   }
                 </div>
                 <div className="p-5 bg-neutral-50 dark:bg-neutral-900 dark:border-t dark:border-neutral-800 flex items-center justify-between">
-                  {loading ?
-                    <div className="flex items-center justify-center w-full ">
-                      <ButtonPrimary>
-                        <svg aria-hidden="true" role="status" className="inline w-8 h-8 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2" />
-                        </svg>
-                      </ButtonPrimary>
-                    </div>
-                    :
-                    <>
-                      <ButtonPrimary
-                        onClick={handleSubmit}
-                        sizeClass="px-4 py-2 sm:px-5"
-                      >
 
-                        Apply
-                      </ButtonPrimary>
-                      <ButtonSecondary sizeClass="px-4 py-2 sm:px-5" onClick={handleDelete}>Delete</ButtonSecondary>
-                      <ButtonThird
-                        onClick={() => {
-                          onClose();
-                        }}
-                        sizeClass="px-4 py-2 sm:px-5"
-                      >
-                        Cancel
-                      </ButtonThird>
-                    </>}
+                  {
+                    buttons[pageState]
+                  }
+
                 </div>
 
               </div>
@@ -412,7 +445,7 @@ const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onCl
           </div>
         </div>
       </Dialog>
-      {openStripeModal && <StripeModal
+      {/* {openStripeModal && <StripeModal
         stripePromise={stripePromise}
         modalIsOpen={openStripeModal}
         onRequestClose={closeStripeModal}
@@ -422,7 +455,7 @@ const DialogPlayerEvent: FC<DialogCoachEventProps> = ({ isOpen, data, onOK, onCl
         cancelPaidProduct={cancelPaidProduct}
         paidTxId={paidTxId}
         members={memberPlayers}
-      />}
+      />} */}
     </>
   )
 }
